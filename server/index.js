1b1c23a3c9a07bef8a5d0736d473039dc1d502d8
bzanetis <britt.zanetis19@gmail.com>
2019-02-21 18:08:21 -0600

    Issues with css but all components rendering

--- server/index.js
+++ server/index.js
@@ -9,9 +9,45 @@ let app = express()
 app.use(express.static(path.join(__dirname, '../')));
 
 
+app.use(function(req, res, next) { 
+
+  res.header('Access-Control-Allow-Origin', '*'); 
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS'); 
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');  
+  //intercepts OPTIONS method 
+  if ('OPTIONS' === req.method){ 
+    res.sendStatus(200); 
+  } else{ 
+    next(); 
+  } 
+}); 
+
+
+
 app.use(
   '/api/movies/details/jurassic-park', cors(), proxy({
     target: 'http://127.0.0.1:3002',
+    changeOrigin: true
+  })
+);
+
+app.use(
+  '/api/movies/*/trailers', cors(), proxy({
+    target: 'http://127.0.0.1:3333',
+    changeOrigin: true
+  })
+);
+
+app.use(
+  '/api/movies/banner', cors(), proxy({
+    target: 'http://127.0.0.1:8080',
+    changeOrigin: true
+  })
+);
+
+app.use(
+  '/api/movies/*/reviews', cors(), proxy({
+    target: 'http://127.0.0.1:4444',
     changeOrigin: true
   })
 );
